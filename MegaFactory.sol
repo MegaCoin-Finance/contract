c04665df4cf61166347fd24916a754def26a643b
tdkhoa2002 <tdkhoa1101@gmail.com>
2023-10-05 09:01:23 +0700

    Deployed Game - Add check: only project owner can create project by native token

--- MegaFactory.sol
+++ MegaFactory.sol
@@ -21,7 +21,7 @@ contract MegaFactoryV1 is CloneFactory, ReentrancyGuard, Ownable {
         address contractGameNew,
         address projectOwner,
         address contractGameOrigin,
-        address contractToken,
+        address contractToken, 
         uint256 deployedFee,
         uint256 affFee,
         uint256 devFee,
@@ -32,10 +32,11 @@ contract MegaFactoryV1 is CloneFactory, ReentrancyGuard, Ownable {
     string private _name = "Mega Factory V1";
     MegaItemsCore public MegaItemsNFT;
     uint256 constant public PERCENTS_DIVIDER = 1000;
+    address public projectWallet = 0x071a5B1451c55153Df15243d0Ff64c8078F75E46;
     address payable public devWallet = payable(0x8b9588F69e04D69655e0d866cD701844177360A7);
     address payable public mktWallet = payable(0x8b9588F69e04D69655e0d866cD701844177360A7);
     address payable public topAddress = payable(0x8b9588F69e04D69655e0d866cD701844177360A7);
-    uint256 public deployedFee = 0.002 ether;
+    uint256 public deployedFee = 0.002 ether; 
     uint256 public affPercent = 100;
     uint256 public devPercent = 200;
     uint256 public sytemFee = 10;
@@ -55,13 +56,16 @@ contract MegaFactoryV1 is CloneFactory, ReentrancyGuard, Ownable {
         MegaItemsNFT = MegaItemsCore(contractNFT);
         listGame[game] = true;
     }
-
+    //0x0000000000000000000000000000000000000000
     function deployedGame(address game, address token, string memory projectId) public payable {
         require(listGame[game] == true, "Game not found");
         require(msg.value >= deployedFee, "Invalid amount");
         require(address(MegaItemsNFT) != address(0), "Contract NFT not found");
         require(validate[_msgSender()][token] == false, "This contract token created by this user");
         require(checkUnique[game][projectId] == false, "This project created");
+        if(token == address(0)) {
+            require(_msgSender() == projectWallet, "Projects cannot be created with native tokens");
+        }
         uint256 affFee = deployedFee.mul(affPercent).div(PERCENTS_DIVIDER);
         uint256 devFee = deployedFee.mul(devPercent).div(PERCENTS_DIVIDER);
         uint256 mktFee = deployedFee.sub(affFee).sub(devFee);
